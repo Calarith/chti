a11627ac2f2c2f15718c2e7c969da5611e033324
DEV <DEV@DEV-PC>
2014-05-23 13:50:59 +0200

    oups

--- PHP/sendMail.php
+++ PHP/sendMail.php
@@ -84,16 +84,3 @@
     }
 
 ?>
-
- $receiver = "cedric.steck@gmail.com";
- $subject = "The subject";
- $message .= 'Hello';
- $header = "MIME-Version: 1.0" . "\r\n";
- $header .= "Content-type:text/html;charset=utf-8" . "\r\n";
- $header .= "From: " .$_POST['from'] . "\r\n";
-
- if(mail($receiver, $subject, $message, $header)){
-     echo "SUCCESS";
- }
- 
-?>
